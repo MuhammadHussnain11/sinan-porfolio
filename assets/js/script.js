4e0ffc2a1bb2df1b4ed5a85737ae0a4f0ac65fef
Muhammad Hussnain <maztarza@gmail.com>
2025-01-21 12:31:20 +0500

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,12 @@ const overlay = document.getElementById('overlay');
 
 // Show the search container
 searchIcon.addEventListener('click', () => {
-    searchContainer.classList.toggle('show');
-    overlay.classList.toggle('show');
-  });
-  
-  // Close the search container using overlay
-  overlay.addEventListener('click', () => {
-    searchContainer.classList.remove('show');
-    overlay.classList.remove('show');
-  });
+  searchContainer.classList.toggle('show');
+  overlay.classList.toggle('show');
+});
+
+// Close the search container using overlay
+overlay.addEventListener('click', () => {
+  searchContainer.classList.remove('show');
+  overlay.classList.remove('show');
+});
